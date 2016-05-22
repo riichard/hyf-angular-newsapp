abe17cdfda87b68ba4fa955a2b75b815d0f6a560
Richard Kraaijenhagen <stdin+git@riichard.com>
2016-05-22 09:51:29 +0200

    Added angular-route angular-resource and got the front page working!

--- app/src/js/routes.js
+++ app/src/js/routes.js
@@ -4,15 +4,15 @@ hyfNews.config(['$routeProvider', function($routeProvider) {
     // Route for the home page
     .when('/', {
         templateUrl: 'pages/index.html',
-        controller: 'mainController'
+        controller: 'myCtrl as newMyCtrl'
     })
     .when('/full/:newsId', {
         templateUrl: 'pages/full.html',
-        controller: 'mainController'
+        controller: 'fullController'
     })
     .when('/category/:categoryId', {
         templateUrl: 'pages/category.html',
-        controller: 'mainController'
+        controller: 'categoryController'
     })
     .otherwise({
         redirectTo: '/'
--- app/src/js/services.js
+++ app/src/js/services.js
@@ -4,14 +4,17 @@ hyfNews.service('news', function($http) {
         return $http.get('/data/news.json')
         .then(function(response) {
             return response.data;
-        })
-    }
+        });
+    };
+});
+
+hyfNews.service('categories', function($http) {
 
     this.getCategories = function() {
         return $http.get('/data/categories.json')
         .then(function(response) {
             return response.data;
-        })
-    }
+        });
+    };
 });
 
