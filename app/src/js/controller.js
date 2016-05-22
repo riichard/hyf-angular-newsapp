b840a84b581353876eff72f769757ad01c3fb775
Richard Kraaijenhagen <stdin+git@riichard.com>
2016-05-22 11:51:47 +0200

    Added articles controller and optionally filters stuff by category id

--- app/src/js/controller.js
+++ app/src/js/controller.js
@@ -9,5 +9,31 @@ var hyfNews = angular.module('hyfNews', ['angular.filter', 'ngResource', 'ngRout
         news.getNews().then(function(articles) {
             ctrl.articles = articles;
         });
+    })
+    .controller('articleController', function($scope, $routeParams) {
+        var articles = $scope.$parent.newMyCtrl.articles;
+        var articleId = $routeParams.articleId;
+        for(var i = 0; i < articles.length; i++) {
+            if( articles[i].news_id == articleId) {
+                $scope.article = articles[i];
+                break;
+            }
+        }
+    })
+    .controller('articlesController', function($scope, $routeParams) {
+        var articles = $scope.$parent.newMyCtrl.articles;
+        var categoryId = $routeParams.categoryId;
+        var displayArticles = [];
+        if(categoryId) {
+            console.log('has category id', categoryId);
+            for(var i = 0; i < articles.length; i++) {
+                if( articles[i].category_id == categoryId) {
+                    displayArticles.push(articles[i]);
+                }
+            }
+        }
+        else {
+            displayArticles = articles;
+        }
+        $scope.displayArticles = displayArticles;
     });
-
